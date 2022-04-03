0eecf1a09653a85b2a65b477eb2f80027dbefd6d
ved7997 <ved7997@gmail.com>
2022-04-03 18:26:10 +0300

    Второй комит 03.04.22

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,8 +1,13 @@
 package ru.skypro;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        System.out.println(nums);
     }
 }
